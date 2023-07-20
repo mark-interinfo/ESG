ee42b860bf4761d258c89d591061bd4e41300ac6
Nick <gdfvsgfd8769@gmail.com>
2023-07-20 13:59:01 +0800

    加入字體

--- src/App.vue
+++ src/App.vue
@@ -10,8 +10,13 @@ import HelloWorld from './components/HelloWorld.vue';
 import CommonHeaderContainer  from './components/CommonHeaderContainer.vue';
 import CommonFooterContainer  from './components/CommonFooterContainer.vue';
 </script>
-<style lang="scss" scoped>
+<style lang="scss">
+@font-face {
+  font-family: 'NotoSans';
+  src: url(../assets/font/NotoSansTC-Regular.otf)format('opentype');
+}
 .body{
+  font-family: NotoSans;
   max-width: 1440px;
   margin: 0 auto;
 }
