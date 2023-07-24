e30e5b7263af01f0d89c186fa0c06ce9cbb02dc7
Nick <gdfvsgfd8769@gmail.com>
2023-07-24 14:18:16 +0800

    header新增下拉選單

--- src/components/CommonHeaderContainer.vue
+++ src/components/CommonHeaderContainer.vue
@@ -4,26 +4,50 @@
       <div id="header-logo">
         <img class="w-100" src="../assets/ESG/Home_img/Homepage_logo.png" alt="">
       </div>
-      <input id="header-search" type="text" placeholder="輸入關鍵字">
+      <div id="header-search">
+        <svg width="32" height="33" viewBox="0 0 32 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M23.9519 23.264L18.758 18.07C19.564 17.028 19.9999 15.754 19.9999 14.414C19.9999 12.81 19.374 11.306 18.242 10.1721C17.11 9.03806 15.602 8.41406 14 8.41406C12.398 8.41406 10.89 9.04006 9.75799 10.1721C8.624 11.304 8 12.81 8 14.414C8 16.016 8.626 17.524 9.75799 18.656C10.89 19.79 12.396 20.414 14 20.414C15.34 20.414 16.612 19.978 17.654 19.174L22.8479 24.366C22.8632 24.3812 22.8812 24.3933 22.9012 24.4016C22.9211 24.4098 22.9424 24.4141 22.9639 24.4141C22.9855 24.4141 23.0068 24.4098 23.0267 24.4016C23.0466 24.3933 23.0647 24.3812 23.0799 24.366L23.9519 23.496C23.9672 23.4808 23.9793 23.4627 23.9875 23.4428C23.9958 23.4229 24 23.4015 24 23.38C24 23.3585 23.9958 23.3371 23.9875 23.3172C23.9793 23.2973 23.9672 23.2792 23.9519 23.264ZM17.168 17.582C16.32 18.428 15.196 18.894 14 18.894C12.804 18.894 11.68 18.428 10.832 17.582C9.98599 16.734 9.51999 15.61 9.51999 14.414C9.51999 13.218 9.98599 12.092 10.832 11.2461C11.68 10.4001 12.804 9.93406 14 9.93406C15.196 9.93406 16.322 10.3981 17.168 11.2461C18.014 12.094 18.48 13.218 18.48 14.414C18.48 15.61 18.014 16.736 17.168 17.582Z" fill="#37D880"/>
+        </svg>
+        <input type="text" placeholder="輸入關鍵字">
+      </div>
     </div>
     <div id="header-middle">
-      <div>
-        <template v-for="(mainLink, index) in linkGroup" :key="mainLink.name">
-          <div class="main-link pointer">
+      <div id="header-links">
+        <template v-for="(mainLink, mainLinkIndex) in linkGroup" :key="mainLink.name">
+          <div
+          class="main-link pointer"
+          @click="showLink(mainLinkIndex)"
+          :class="{'selected': mainLinkIndex === isLinkShow}"
+          >
             <span>
               {{ mainLink.name }}
             </span>
             <svg v-if="mainLink.children" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M6 9.5L11.7778 14.5L18 9.5" stroke="#272727" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
+              <path d="M6 9.5L11.7778 14.5L18 9.5" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
             </svg>
+            <div
+            class="dropdown"
+            :class="{'show': mainLinkIndex === isLinkShow}"
+            v-if="mainLink.children"
+            >
+              <template
+              v-for="(subLink, subLinkIndex) in mainLink.children"
+              :key="subLink"
+              >
+                <div class="sub-link">
+                  {{ subLink }}
+                </div>
+                <div class="line" v-if="subLinkIndex < mainLink.children.length - 1"></div>
+              </template>
+            </div>
           </div>
-          <div v-if="index !== linkGroup.length-1" class="gap-line"></div>
+          <div v-if="mainLinkIndex < linkGroup.length - 1" class="gap-line"></div>
         </template>
       </div>
       <router-link to="HomeView">
-        <svg class="pointer" width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M16 8C15.0111 8 14.0444 8.29325 13.2222 8.84265C12.3999 9.39206 11.759 10.173 11.3806 11.0866C11.0022 12.0002 10.9031 13.0055 11.0961 13.9755C11.289 14.9454 11.7652 15.8363 12.4645 16.5355C13.1637 17.2348 14.0546 17.711 15.0246 17.9039C15.9945 18.0969 16.9998 17.9978 17.9134 17.6194C18.8271 17.241 19.6079 16.6001 20.1574 15.7779C20.7068 14.9556 21 13.9889 21 13C21 11.6739 20.4732 10.4021 19.5355 9.46447C18.5979 8.52678 17.3261 8 16 8ZM16 16C15.4067 16 14.8266 15.8241 14.3333 15.4944C13.8399 15.1648 13.4554 14.6962 13.2284 14.1481C13.0013 13.5999 12.9419 12.9967 13.0576 12.4147C13.1734 11.8328 13.4591 11.2982 13.8787 10.8787C14.2982 10.4591 14.8328 10.1734 15.4147 10.0576C15.9967 9.94189 16.5999 10.0013 17.1481 10.2284C17.6962 10.4554 18.1648 10.8399 18.4944 11.3333C18.8241 11.8266 19 12.4067 19 13C18.9992 13.7954 18.6829 14.558 18.1204 15.1204C17.558 15.6829 16.7954 15.9992 16 16Z" fill="#272727"/>
-          <path d="M16 2C13.2311 2 10.5243 2.82109 8.22202 4.35943C5.91973 5.89777 4.12532 8.08427 3.06569 10.6424C2.00607 13.2006 1.72882 16.0155 2.26901 18.7313C2.80921 21.447 4.14258 23.9416 6.10051 25.8995C8.05845 27.8574 10.553 29.1908 13.2687 29.731C15.9845 30.2712 18.7994 29.9939 21.3576 28.9343C23.9157 27.8747 26.1022 26.0803 27.6406 23.778C29.1789 21.4757 30 18.7689 30 16C29.9958 12.2883 28.5194 8.72977 25.8948 6.10518C23.2702 3.48059 19.7117 2.00423 16 2ZM10 26.377V25C10.0008 24.2046 10.3171 23.442 10.8796 22.8796C11.442 22.3171 12.2046 22.0008 13 22H19C19.7954 22.0008 20.558 22.3171 21.1204 22.8796C21.6829 23.442 21.9992 24.2046 22 25V26.377C20.1792 27.4402 18.1085 28.0005 16 28.0005C13.8915 28.0005 11.8208 27.4402 10 26.377ZM23.992 24.926C23.9721 23.6143 23.4377 22.363 22.5039 21.4416C21.5702 20.5202 20.3118 20.0025 19 20H13C11.6882 20.0025 10.4298 20.5202 9.4961 21.4416C8.56235 22.363 8.02794 23.6143 8.00801 24.926C6.19457 23.3067 4.91574 21.1749 4.34084 18.8127C3.76594 16.4505 3.92211 13.9693 4.78864 11.6979C5.65518 9.42637 7.19123 7.47167 9.19338 6.09257C11.1955 4.71347 13.5693 3.97503 16.0005 3.97503C18.4317 3.97503 20.8055 4.71347 22.8076 6.09257C24.8098 7.47167 26.3458 9.42637 27.2124 11.6979C28.0789 13.9693 28.2351 16.4505 27.6602 18.8127C27.0853 21.1749 25.8054 23.3067 23.992 24.926Z" fill="#272727"/>
+        <svg width="40" height="41" viewBox="0 0 40 41" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <circle cx="20" cy="20.4141" r="13.4" fill="#808080" stroke="#808080" stroke-width="1.2"/>
+          <path d="M13.75 27.9141C13.75 27.9141 12.5 27.9141 12.5 26.6641C12.5 25.4141 13.75 21.6641 20 21.6641C26.25 21.6641 27.5 25.4141 27.5 26.6641C27.5 27.9141 26.25 27.9141 26.25 27.9141H13.75ZM20 20.4141C20.9946 20.4141 21.9484 20.019 22.6517 19.3157C23.3549 18.6125 23.75 17.6586 23.75 16.6641C23.75 15.6695 23.3549 14.7157 22.6517 14.0124C21.9484 13.3092 20.9946 12.9141 20 12.9141C19.0054 12.9141 18.0516 13.3092 17.3483 14.0124C16.6451 14.7157 16.25 15.6695 16.25 16.6641C16.25 17.6586 16.6451 18.6125 17.3483 19.3157C18.0516 20.019 19.0054 20.4141 20 20.4141Z" fill="white"/>
         </svg>
       </router-link>
     </div>
@@ -52,32 +76,55 @@ import { ref } from 'vue';
 const linkGroup = ref([
   {
     name: '系統管理',
-    children: []
+    children: [
+      '模組管理',
+      '監控單位建立',
+      '系統參數維護'
+    ]
   },
   {
     name: '帳號與權限',
-    children: []
+    children: [
+      '監控單位使用者建立',
+      '申報單位權限設定',
+    ]
   },
   {
     name: '指標維護',
-    children: []
-  },
-  {
-    name: 'XBRL維護',
-    children: []
+    children: [
+      '證交所核心指標設定',
+      '國際準則指標設定',
+      'ESG資訊矩陣設定',
+    ]
   },
   {
     name: '申報管理',
-    children: []
+    children: [
+      '更正審核作業',
+      '申報情形控管表',
+    ]
   },
   {
     name: '申報作業',
-    children: []
+    children: [
+      'ESG資料申報',
+      '永續報告書套表',
+      '永續報告書申報',
+      '申請資料更正',
+    ]
   },
   {
     name: 'ESG資訊下載',
   }
 ]);
+const isLinkShow = ref(-1);
+const showLink = function(i){
+  if(i === isLinkShow.value){
+    isLinkShow.value = -1;
+  } else {
+    isLinkShow.value = i;
+  }
+}
 </script>
 <style lang="scss" scoped>
 // common setting
@@ -101,7 +148,24 @@ header{
       width: 284px;
     }
     #header-search{
+      display: flex;
+      align-items: center;
+      width: 240px;
+      height: 48px;
+      border: 1px solid #EFEFEF;
+      color: #BEBEBE;
       padding: 0px 12px;
+      font-size: 16px;
+      border-radius: 3px;
+      input{
+        border: 0px;
+        font-size: 16px;
+        color: #BEBEBE;
+        &:focus{
+          border: 0px;
+          outline: none;
+        }
+      }
     }
   }
   #header-middle{
@@ -109,16 +173,53 @@ header{
     align-items: center;
     justify-content: space-between;
     padding: 0 40px;
-    .main-link{
-      display: inline-flex;
+    #header-links{
+      display: flex;
       align-items: center;
-      padding: 18px 30px;
-    }
-    .gap-line{
-      display: inline-block;
-      height: 24px;
-      width: 1px;
-      background: #BEBEBE;
+      .main-link{
+        display: inline-flex;
+        align-items: center;
+        gap: 4px;
+        padding: 18px 30px;
+        position: relative;
+        path{
+          stroke: #272727;
+        }
+        &.selected{
+          color: #37D880;
+          path{
+            stroke: #37D880;
+          }
+        }
+        .dropdown{
+          display: none;
+          position: absolute;
+          top: calc(100% + 1px);
+          left: 50%;
+          width: 100%;
+          background: #fff;
+          border: 1px solid #ddd;
+          transform: translate(-50%, 0%);
+          &.show{
+            display: block;
+          }
+          .sub-link{
+            padding: 12px 16px;
+            color: #272727;
+          }
+          .line{
+            width: 100%;
+            height: 1px;
+            background: #eee;
+          }
+        }
+      }
+      .gap-line{
+        display: inline-block;
+        height: 24px;
+        width: 1px;
+        background: #BEBEBE;
+      }
     }
   }
   #header-bottom{
