c7cc80c30ff12349cd0f284aa5dd5fc328723b96
mark-interinfo <mark@interinfo.com.tw>
2023-08-01 10:57:24 +0800

    dist

--- build.js
+++ build.js
@@ -5,28 +5,27 @@ import path from 'path';
 function deleteDir(url){
     var files = [];
         
-    if( fs.existsSync(url) ) {  //判断给定的路径是否存在
+    if( fs.existsSync(url) ) {  //判斷給定的路徑是否存在
             
-        files = fs.readdirSync(url);   //返回文件和子目录的数组
+        files = fs.readdirSync(url);   //返回文件和子目錄的數組
         files.forEach(function(file,index){
             var curPath = path.join(url,file);
                 
-            if(fs.statSync(curPath).isDirectory()) { //同步读取文件夹文件，如果是文件夹，则函数回调
+            if(fs.statSync(curPath).isDirectory()) { //同步讀取文件夾文件，如果是文件夾，則函數回調
                 deleteDir(curPath);
             } else {    
-                fs.unlinkSync(curPath);    //是指定文件，则删除
+                fs.unlinkSync(curPath);    //是指定文件，則刪除
             }
                 
         });
             
-        fs.rmdirSync(url); //清除文件夹
+        fs.rmdirSync(url); //清除文件夾
     }else{
-        console.log("不存在！");
+        console.log("資料夾不存在！");
     }
-
 }
 
-deleteDir('c:/dmaker64/html/css/ESG/assets');
+deleteDir('../assets');
 
 const __filename = fileURLToPath(import.meta.url);
 
@@ -44,3 +43,4 @@ const dist = path.resolve(__dirname, './dist/assets');
 const templateDist= path.resolve(__dirname, '../assets');
 
 fs.renameSync(dist, templateDist);
+
