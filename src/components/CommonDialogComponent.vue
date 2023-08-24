f36fd1f5ea9884aa07f36978555e874d15d288b4
mark-interinfo <mark@interinfo.com.tw>
2023-08-24 17:58:06 +0800

    u

--- src/components/CommonDialogComponent.vue
+++ src/components/CommonDialogComponent.vue
@@ -220,7 +220,7 @@
     <!-- 顯示圖片 -->
     <!-- EsgTemplate.vue -->
     <div id="dialog-block" class="displayImg" v-if="props.dailogType == 'img'">
-      <img src="../assets/images/template1.png">
+      <img src="../assets/images/template1.jpg">
     </div>
 
     <!-- 上傳進度條 -->
